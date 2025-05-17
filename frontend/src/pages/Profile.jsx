d28eb7f7b264017362c16148fb0edaf660cffca7
Mithunyadav291 <yadavmithun291@gmail.com>
2025-05-17 21:11:44 +0545

    second commit

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Sidebar from '../components/Profile/sidebar'
+import Sidebar from '../components/Profile/Sidebar'
 import { Outlet } from 'react-router-dom'
 import axios from 'axios'
 import Loader from '../components/Loader/Loader'
